be3894bbf5bc27536e578b4602cbfa59da430eb4
TRACE-PC\Trace <tracerainbolt@gmail.com>
2017-05-16 22:10:35 -0700

    Cleaned up code.

--- LC3main.py
+++ LC3main.py
@@ -1,71 +1,60 @@
-import sys
-import os
-import copy
-from threading import Thread
-from time import sleep
-import errno
 import msvcrt
-import numpy as np
-from storage import *
+import sys
+
 import control_unit as ctu
 import instruction_parser as parser
+from storage import *
 
-#Initialize registers
-regs = registers(np.array([0,0,0,0,0,0,0,0], dtype='int16'), 0, 0, 0)
+# Initialize registers
+regs = registers(np.array([0, 0, 0, 0, 0, 0, 0, 0], dtype='int16'), 0, 0, 0)
 
-#Initialize Memory, turn ON machine
+# Initialize Memory, turn ON machine
 memory = memory()
 ON = True
 
-#Useful memory locations
-KBSR = -512 #xFE00
-KBDR = -510 #xFE02
-DSR = -508  #xFE04
-DDR = -506  #xFE06
-MCR = -2  #xFFFE
+# Useful memory locations
+KBSR = -512  # 0xFE00
+KBDR = -510  # 0xFE02
+DSR = -508  # 0xFE04
+DDR = -506  # 0xFE06
+MCR = -2  # 0xFFFE
 
-#Location of OS file
+# Location of OS file
 os_file_name = "operating_sys_lc3.txt"
 
-#Main function, intializes memory and starts running isntructions
-def main():
-  memory.memory = memory.load_os(os_file_name, 65536)
-  memory.load_instructions('instructs/data.txt', regs)
-  #memory.load_instructions('instructs/trap.txt', regs)
-  memory.load_instructions('instructs/quiz.txt', regs)
-  memory[MCR] = 0xFFFF
-  run_instructions()
-  print ''
-  regs.print_registers()
-  print ''
-  regs.print_spec_regs()
-  #print bin(memory[KBSR])
 
-#Handles basics for running instructions
+# Main function, initializes memory and starts running instructions
+def main():
+    memory.memory = memory.load_os(os_file_name, 65536)
+    memory.load_instructions('instructions/data.txt', regs)
+    memory.load_instructions('instructions/quiz.txt', regs)
+    memory[MCR] = 0xFFFF
+    run_instructions()
+    print ''
+    regs.print_registers()
+    print ''
+    regs.print_spec_regs()
+
+
+# Handles basics for running instructions
 def run_instructions():
-    cycle = 0
     while (memory[MCR] >> 15) & 0b1 == 1:
-    #for i in range(720):
-    #while True:
+        # for i in range(720):
+        # while True:
         regs.PC += 1
         inst = memory[regs.PC - 1]
         regs.IR = inst
-        #print to_hex_string(regs.PC - 1) + " " + to_hex_string(regs.IR) + " " + to_hex_string(regs.CC) + " " + to_hex_string(regs.registers[0])
+        # print to_hex_string(regs.PC - 1) + " " + to_hex_string(regs.IR) + " " + to_hex_string(regs.CC) + " " +
+        # to_hex_string(regs.registers[0])
         handle_instruction(inst)
-        if ON == False:
-            if regs.PC == sign_extend(0xFD79, 16):
-                memory[MCR] = 0x7FFF
+        if not ON and regs.PC == sign_extend(0xFD79, 16):
+            memory[MCR] = 0x7FFF
 
-        #poll_status_registers()
-        #print to_hex_string(regs.PC)
 
-#Finds instruction and tells handle to execute it
+# Finds instruction and tells handle to execute it
 def handle_instruction(inst):
     opcode = inst >> 12
-    inst_list = []
     str_op = parse_op(opcode)
-    reg_state = copy.deepcopy(regs.registers)
-    temp = None
     if str_op == 'ADD':
         handle_add(inst)
     elif str_op == 'NOT':
@@ -98,18 +87,19 @@ def handle_instruction(inst):
         handle_trap(inst)
 
 
-#Detect that a key has been pressed:
-def kbfunc(): 
-   x = msvcrt.kbhit()
-   if x: 
-      ret = ord(msvcrt.getch()) 
-   else: 
-      ret = None 
-   return ret
+# Detect that a key has been pressed:
+def kb_func():
+    x = msvcrt.kbhit()
+    if x:
+        ret = ord(msvcrt.getch())
+    else:
+        ret = None
+    return ret
 
-#See if status registers have been updated
+
+# See if status registers have been updated
 def poll_status_registers():
-    char = kbfunc()
+    char = kb_func()
     if char:
         memory[KBSR] = memory[KBSR] + 0x8000
         if char == 0x0D:
@@ -118,22 +108,27 @@ def poll_status_registers():
         return True
     return False
 
-#For future use (when threads are added)
+
+# For future use (when threads are added)
 def update_keyboard():
     while ON:
         poll_status_registers()
 
-#Handle the Dispaly Data Register, called when updated
+
+# Handle the Display Data Register, called when updated
 def handle_DDR():
     if (memory[DSR] >> 15) & 0b1 == 1:
         if memory[DDR] in range(256):
             sys.stdout.write(chr(memory[DDR]))
 
-#Handle the Keyboard Status Register, called when updated
+
+# Handle the Keyboard Status Register, called when updated
 def handle_KBSR():
     if (memory[KBSR] >> 15) & 1 == 1:
-        memory[KBSR] = memory[KBSR] & 0x4000  #Reset KBSR
+        memory[KBSR] = memory[KBSR] & 0x4000  # Reset KBSR
     poll_status_registers()
+
+
 #
 # HANDLERS: THe following functions handle
 # their respective instructions
@@ -141,14 +136,14 @@ def handle_KBSR():
 def handle_add(inst):
     inst_list = parser.parse_add(inst)
     V2 = 0
-    #Check for imm
-    if inst_list[3] == 0: 
+    # Check for imm
+    if inst_list[3] == 0:
         V2 = regs[inst_list[4]]
     else:
         V2 = sign_extend(inst_list[4], 5)
     DR = inst_list[1]
     SR1 = regs[inst_list[2]]
-    #Let fsm execute add
+    # Let fsm execute add
     inst_list_eval = [SR1, V2]
     value = ctu.execute_add(inst_list_eval)
     n = regs.registers[3]
@@ -164,21 +159,23 @@ def handle_not(inst):
     regs.registers[DR] = value
     regs.set_CC(value)
 
+
 def handle_and(inst):
     inst_list = parser.parse_add(inst)
     V2 = 0
-    #Check for imm
-    if inst_list[3] == 0: 
+    # Check for imm
+    if inst_list[3] == 0:
         V2 = regs[inst_list[4]]
     else:
         V2 = sign_extend(inst_list[4], 5)
     DR = inst_list[1]
     SR1 = regs[inst_list[2]]
-    #Let fsm execute add
+    # Let fsm execute add
     inst_list_eval = [SR1, V2]
     value = ctu.execute_and(inst_list_eval)
-    regs.registers[DR]  = value
+    regs.registers[DR] = value
     regs.set_CC(value)
+
 
 def handle_ld(inst):
     inst_list = parser.parse_ld(inst)
@@ -190,6 +187,7 @@ def handle_ld(inst):
         handle_KBSR()
     regs.registers[DR] = value
 
+
 def handle_ldi(inst):
     inst_list = parser.parse_ldi(inst)
     DR = inst_list[1]
@@ -199,6 +197,7 @@ def handle_ldi(inst):
     if memory[address] == KBSR:
         handle_KBSR()
     regs.registers[DR] = value
+
 
 def handle_ldr(inst):
     inst_list = parser.parse_ldr(inst)
@@ -211,11 +210,13 @@ def handle_ldr(inst):
     regs.registers[DR] = value
     regs.set_CC(value)
 
+
 def handle_lea(inst):
     inst_list = parser.parse_lea(inst)
     DR = inst_list[1]
     address = regs.PC + sign_extend(inst_list[2], 9)
     regs.registers[DR] = address
+
 
 def handle_st(inst):
     inst_list = parser.parse_st(inst)
@@ -226,6 +227,7 @@ def handle_st(inst):
     if val == DDR:
         handle_DDR()
 
+
 def handle_sti(inst):
     inst_list = parser.parse_st(inst)
     SR = inst_list[1]
@@ -233,6 +235,7 @@ def handle_sti(inst):
     memory[memory[address]] = regs.registers[SR]
     if memory[address] == DDR:
         handle_DDR()
+
 
 def handle_str(inst):
     inst_list = parser.parse_str(inst)
@@ -243,6 +246,7 @@ def handle_str(inst):
     if regs.registers[SR] == DDR:
         handle_DDR()
 
+
 def handle_br(inst):
     inst_list = parser.parse_br(inst)
     condition = '{:03b}'.format(inst_list[1])
@@ -252,33 +256,34 @@ def handle_br(inst):
         if bit == "1" and bit == cc[i]:
             regs.PC = address
 
+
 def handle_jsr(inst):
     inst_list = parser.parse_jsr(inst)
     address = 0x0000
-    if inst_list[1] == 1: #JSR
+    if inst_list[1] == 1:  # JSR
         address = regs.PC + sign_extend(inst_list[2], 11)
-    else: #JSRR 
+    else:  # JSRR
         BaseR = inst_list[2]
         address = regs.registers[BaseR]
     regs.registers[7] = regs.PC
     regs.PC = address
 
+
 def handle_ret(inst):
     inst_list = parser.parse_ret(inst)
     regs.PC = regs.registers[7]
 
+
 def handle_rti(inst):
-    inst_list = parser.parse_ret(inst)
     if regs.PSR >> 15 == 0:
         regs.PC = memory[regs.registers[6]]
         regs.registers[6] += 1
         temp = memory[regs.registers[6]]
         regs.registers[6] += 1
         regs.PSR = temp
-        regs.CC = PSR & 0b111
+        regs.CC = regs.PSR & 0b111
     else:
-        print "Priviledge mode exception."
-        ON = False1
+        print "Privilege mode exception."
 
 
 def handle_trap(inst):
@@ -290,26 +295,29 @@ def handle_trap(inst):
         ON = False
 
 
-#Sign extend, used for SEXTing offsets
+# Sign extend, used for SEXTing offsets
 def sign_extend(val, bits):
-    if (val & (1 << (bits - 1))) != 0: # if sign bit is set
-        val = val - (1 << bits)        # compute negative value
-    return val                         # return positive value as is
+    if (val & (1 << (bits - 1))) != 0:  # if sign bit is set
+        val = val - (1 << bits)  # compute negative value
+    return val  # return positive value as is
 
-#Return opcode as string
+
+# Return opcode as string
 def parse_op(opcode):
-    return ['BR','ADD','LD', 'ST', 
-           'JSR','AND','LDR','STR', 
-           'RTI','NOT', 'LDI', 'STI',
-           'RET','NOP','LEA','TRAP'][opcode]
+    return ['BR', 'ADD', 'LD', 'ST',
+            'JSR', 'AND', 'LDR', 'STR',
+            'RTI', 'NOT', 'LDI', 'STI',
+            'RET', 'NOP', 'LEA', 'TRAP'][opcode]
 
-#Used to print hex values in proper format
+
+# Used to print hex values in proper format
 def to_hex_string(val):
     return 'x' + '{:04x}'.format((val + (1 << 16)) % (1 << 16)).upper()
- 
+
+
 if __name__ == '__main__':
     main()
-    #thread_main = Thread(target = main)
+    # thread_main = Thread(target = main)
     # thread_keyb = Thread(target = update_keyboard)
     '''thread_main.daemon = True
     thread_keyb.daemon = True
@@ -317,4 +325,3 @@ if __name__ == '__main__':
     thread_keyb.start()
     thread_main.join()
     thread_keyb.join()'''
- 
--- storage.py
+++ storage.py
@@ -1,42 +1,44 @@
 import numpy as np
 
-#Register class for registers, PC, IR, and CC
+# Register class for registers, PC, IR, and CC
 class registers:
-  def __init__(self, registers, PC, IR, CC):
-      self.registers = registers
-      self.PC = PC
-      self.IR = IR
-      self.CC = CC
-      self.PSR = 0x8000 + self.CC
-      self.iPC = PC + 1
+    def __init__(self, registers, PC, IR, CC):
+        self.registers = registers
+        self.PC = PC
+        self.IR = IR
+        self.CC = CC
+        self.PSR = 0x8000 + self.CC
 
-  def __getitem__(self, position):
-      return self.registers[position]
+    def __getitem__(self, position):
+        return self.registers[position]
 
-  #Handles .ORIG line
-  def set_origin(self, origin):
-      self.PC = origin
+    # Handles .ORIG line
+    def set_origin(self, origin):
+        self.PC = origin
 
-  def set_CC(self, value):
-      if value < 0:
-          self.CC = 0b100
-      elif value == 0:
-          self.CC = 0b010
-      elif value > 0:
-          self.CC = 0b001
-      else:
-          print "Invalde CC state entered."
+    def set_CC(self, value):
+        self.PSR = 0x8000 + self.CC
+        if value < 0:
+            self.CC = 0b100
+        elif value == 0:
+            self.CC = 0b010
+        elif value > 0:
+            self.CC = 0b001
+        else:
+            print "Invalid CC state entered."
 
-  def print_registers(self):
-      for i, register in enumerate(self.registers):
-          print "R" + str(i) + ": " + to_hex_string(register)
+    def print_registers(self):
+        for i, register in enumerate(self.registers):
+            print "R" + str(i) + ": " + to_hex_string(register)
 
-  def print_spec_regs(self):
-      print "PC: " + to_hex_string(self.PC)
-      print "IR: " + to_hex_string(self.IR)
-      print "CC: " + '{:03b}'.format(self.CC)
+    def print_spec_regs(self):
+        print "PC: " + to_hex_string(self.PC)
+        print "IR: " + to_hex_string(self.IR)
+        print "PSR:" + to_hex_string(self.PSR)
+        print "CC: " + '{:03b}'.format(self.CC)
 
-#Memory class: xFFFF memory locations, containing a singed 16 bit number
+
+# Memory class: xFFFF memory locations, containing a singed 16 bit number
 class memory:
     def __init__(self, memory=None):
         self.memory = memory
@@ -48,8 +50,6 @@ class memory:
         self.memory[position] = item
 
     def load_instructions(self, fname, regs):
-        first = True
-        orig = 0x0000
         with open(fname) as f:
             orig = int("".join(f.readline()), 2)
             for i, line in enumerate(f):
@@ -57,7 +57,7 @@ class memory:
                 i += 1
         regs.set_origin(orig)
 
-    #Loads LC3 Operating System from text file
+    # Loads LC3 Operating System from text file
     def load_os(self, fname, nrow):
         x = np.empty(nrow, dtype='int16')
         with open(fname) as f:
@@ -65,12 +65,14 @@ class memory:
                 x[irow] = int(line, 2)
         return x
 
-#Used for properly formatting/printing hex numbers
+
+# Used for properly formatting/printing hex numbers
 def to_hex_string(val):
     return 'x' + '{:04x}'.format((val + (1 << 16)) % (1 << 16)).upper()
 
-#Sign extend, used for SEXTing offsets
+
+# Sign extend, used for SEXTing offsets
 def sign_extend(val, bits):
-    if (val & (1 << (bits - 1))) != 0: # if sign bit is set
-        val = val - (1 << bits)        # compute negative value
-    return val                         # return positive value as is
+    if (val & (1 << (bits - 1))) != 0:  # if sign bit is set
+        val = val - (1 << bits)  # compute negative value
+    return val  # return positive value as is
